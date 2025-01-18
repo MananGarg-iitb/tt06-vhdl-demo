bb5eb0edda32aa4bd49719cd8e47aa04b9d0ea11
MananGarg-iitb <144512698+MananGarg-iitb@users.noreply.github.com>
2025-01-18 20:37:16 +0530

    Update top.vhdl

--- src/top.vhdl
+++ src/top.vhdl
@@ -1,9 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_vhdl_seven_segment_seconds is
-    generic (MAX_COUNT : natural := 10_000_000);
+entity tt_um_full_adder is
     port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
@@ -14,45 +12,18 @@ entity tt_um_vhdl_seven_segment_seconds is
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
-end tt_um_vhdl_seven_segment_seconds;
+end tt_um_full_adder;
 
-architecture Behavioral of tt_um_vhdl_seven_segment_seconds is
-    signal reset          : std_logic;
-    signal led_out        : std_logic_vector(6 downto 0);
-    signal second_counter : std_logic_vector(23 downto 0) := (others => '0');
-    signal digit          : std_logic_vector(3 downto 0) := (others => '0');
-    signal compare        : std_logic_vector(23 downto 0);
+architecture Behavioral of tt_um_full_adder is
+    signal temp : std_logic_vector(15 downto 0);
 begin
 
-    reset <= not rst_n;
-    uo_out(6 downto 0) <= led_out;
-    uo_out(7) <= '0';
-    uio_oe <= (others => '1');
-    uio_out <= std_logic_vector(second_counter(7 downto 0));
+    assign uo_out(7) <= (ui_in(7) xor ui_in(6)) xor ui_in(5);
+    assign uo_out(6) <= ((ui_in(7) and ui_in(6)) or (ui_in(6) and ui_in(5))) or (ui_in(7) and ui_in(5));
 
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if reset = '1' then
-                second_counter <= (others => '0');
-                digit <= (others => '0');
-            else
-                if second_counter = compare then
-                    second_counter <= (others => '0');
-                    if digit = "1001" then
-                        digit <= (others => '0');
-                    else
-                        digit <= std_logic_vector(unsigned(digit) + 1);
-                    end if;
-                else
-                    second_counter <= std_logic_vector(unsigned(second_counter) + 1);
-                end if;
-            end if;
-        end if;
-    end process;
+    assign uo_out(5 downto 0) <= "000000";
+    assign uio_out(7 downto 0) <= "00000000";
+    assign uio_oe(7 downto 0) <= "00000000";
 
-    compare <= "000000" & ui_in & "0000000000" when ui_in /= "00000000" else std_logic_vector(to_unsigned(MAX_COUNT, 24));
-
-    seg7_inst : entity work.seg7 port map(counter => digit, segments => led_out);
-
+    assign temp <= ui_in(4 downto 0) & uio_in & ena & clk & rst_n ;
 end Behavioral;
